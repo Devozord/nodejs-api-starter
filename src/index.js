6df798965028164eb1703b4a5048997bc2732ff7
xeewi <xwlmn77@gmail.com>
2018-02-25 03:27:58 +0100

    refactor(index): remove view rendering
    Removing view rendering who is useless for a JSON API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
-import path from 'path'
 import config from '../config/config.json'
 import * as errorHandler from './core/errorHandler'
 
@@ -17,15 +15,12 @@ dotenv.load({ path: `./config/.env.${process.env.NODE_ENV || 'development'}` })
 // configure express app
 const app = express()
 app.set('port', process.env.PORT || 5000)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'twig')
 
 // Middlewares
 app.use(logger('dev'))
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
 
 // Here you can put all your routes controllers:
 app.use(config.prefix + '/account', account)
